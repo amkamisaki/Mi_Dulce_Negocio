708dcd8aa96161c8c3388f4b8b67bc4bc1bc7a96
Nefi <amkamisaki@hotmail.com>
2021-06-10 21:27:03 -0300

    Firebase config

---
+++ app/src/main/java/Admin.java
@@ -0,0 +1,20 @@
+public class Admin {
+
+    private int id;
+    private String name;
+    private String password;
+
+   /* public receiveOrder (Order order) {
+        order.status = "received";
+    }
+    public rejectOrder (Order order) {
+        order.status = "rejected";
+    }
+    public sendOrder (Order order) {
+        order.status = "sent";
+    }
+    public finishOrder(Order order) {
+        order.status = "finished"
+    }*/
+
+}
