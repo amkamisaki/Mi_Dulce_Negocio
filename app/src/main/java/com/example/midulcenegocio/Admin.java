7d74921e9b528346ba091fd749aa8eb0d2e14312
amkamisaki <55413941+amkamisaki@users.noreply.github.com>
2021-06-17 20:35:10 -0300

    Add files via upload
    Added java classes

---
+++ app/src/main/java/com/example/midulcenegocio/Admin.java
@@ -0,0 +1,23 @@
+package com.example.midulcenegocio;
+
+public class Admin {
+
+    private int id;
+    private String name;
+    private String password;
+   /* private Order order
+
+   public receiveOrder (order) {
+        order.status = "received";
+    }
+    public rejectOrder (order) {
+        order.status = "rejected";
+    }
+    public sendOrder (order) {
+        order.status = "sent";
+    }
+    public finishOrder(order) {
+        order.status = "finished"
+    }*/
+
+}
--- app/src/main/java/com/example/midulcenegocio/MainActivity.java
+++ app/src/main/java/com/example/midulcenegocio/MainActivity.java
@@ -1,14 +1,135 @@
 package com.example.midulcenegocio;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private Button btnCancel;
+    private Button btnSignIn;
+    private EditText edtEmail;
+    private EditText edtPassword;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+        btnSignIn = (Button) findViewById(R.id.btnSignIn);
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+        edtPassword = (EditText) findViewById(R.id.edtPassword);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser(edtEmail.getText().toString(), edtPassword.getText().toString());
+            }
+        });
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edtEmail.setText("");
+                edtPassword.setText("");
+
+            }
+        });
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addUser(edtEmail.getText().toString(), edtPassword.getText().toString());
+            }
+        });
     }
+
+    private void loginUser (String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(MainActivity.this, "Login success", Toast.LENGTH_SHORT).show();
+
+                            openFirstActivity();
+                            } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void addUser (String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(MainActivity.this, "New account created with success", Toast.LENGTH_SHORT).show();
+
+                            openFirstActivity();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+
+    private Boolean userConnected() {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (userConnected()) {
+            openFirstActivity();
+        }
+    }
+
+    private void openFirstActivity() {
+
+        Intent intent = new Intent(getApplicationContext(), FirstActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
